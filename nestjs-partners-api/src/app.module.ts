7ac6f86b53bf691ff6744fa7b92ca05bc455a2fd
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2024-07-08 11:54:33 -0300

    feat: base code for rest api endpoint spots

--- nestjs-partners-api/src/app.module.ts
+++ nestjs-partners-api/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EventModule } from './event/event.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { SpotsModule } from './spots/spots.module';
 
 @Module({
-  imports: [EventModule, PrismaModule],
+  imports: [EventModule, PrismaModule, SpotsModule],
   controllers: [AppController],
   providers: [AppService],
 })
